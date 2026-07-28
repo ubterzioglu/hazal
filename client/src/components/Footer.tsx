bfd26ad8dfea99e01e0994c2b9efd30a6dc53d7d
Umut Baris Terzioglu <Umut.Baris-Terzioglu@adesso-insurance-solutions.de>
2026-07-28 08:35:32 +0200

    .

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -7,6 +7,7 @@ export default function Footer() {
         </a>{" "}
         <span>Firması Ufuksoy Nakliyat A.Ş</span>
         <span><a href="https://tekhurdametal.com/hurda-demir-fiyatlari/" rel="dofollow" className="hover:text-slate-600 transition-colors">Demir Hurda Fiyatları</a> Tek Hurda Metal</span>
+        <span><a href="https://lionerotik.com/urunler/fetis-urunleri" rel="dofollow" className="hover:text-slate-600 transition-colors">Antalya Seks Shop</a> Lion Erotik</span>
       </div>
     </footer>
   );
